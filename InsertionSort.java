deb3e4542f30a04ffb2b65ab85f788d0cff274d1
PavloLuchyk <76796482+PavloLuchyk@users.noreply.github.com>
2020-12-31 14:12:07 +0200

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -8,7 +8,7 @@ class InsertionSort implements ISort{
         for(int i=1;i<arr.length;i++){
             key=arr[i];
             j=i-1;
-            while (j>=0&&arr[j]>key){
+            while (j>=0&&arr[j] < key){
                 arr[j+1]=arr[j];
                 j--;
             }
